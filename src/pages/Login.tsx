9a88b02627b2b2fdc4928e1eead0cb80738b60cf
Alberto <albertojunior848423803@gmail.com>
2025-02-10 23:02:37 +0300

    c

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -17,9 +17,9 @@ export const Login = () => {
   };
 
   return (
-    <Box maxW="400px" mx="auto" mt={10}>
+    j < Box maxW = "400px" mx = "auto" mt = { 10} >
       <Heading mb={6}>Login</Heading>
       <AuthForm isLogin onSubmit={handleLogin} />
-    </Box>
+    </Box >
   );
 };
